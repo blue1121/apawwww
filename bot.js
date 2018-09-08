99dad706c78390f987274e80f09d34fa657780df
blue1121 <43077735+blue1121@users.noreply.github.com>
2018-09-08 14:00:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = 'm-'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`m-help | MusicBot :blue_heart: :sparkles:`,"http://twitch.tv/S-F")
+client.user.setGame(`m-help | MusicBot 💙✨`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
