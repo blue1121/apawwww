7fa5d80e5850a728e2168d1bbd1f10b9c84b4a76
blue1121 <43077735+blue1121@users.noreply.github.com>
2018-12-07 04:54:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -450,5 +450,42 @@ cmds.repeat = {
     }
 };
 
+const devs = ['349095859859881984'];
+const child_process = require("child_process");
+///restart
+client.on('message', message => {
+if(message.content === prefix + "restart") {
+      if (!devs.includes(message.author.id)) return;;
+        console.log("⚠️ جاري اعادة تشغيل البوت... ⚠️");
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log("تم اعادة تشغيل البوت");
+    }
+  
+  });
+
+const adminprefix = "-";
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'avatarr')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
 
 client.login(process.env.BOT_TOKEN);
